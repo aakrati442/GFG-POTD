c735c0db66ee72c7d97ac0e7e106dca49bcd1e17
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-09-28 17:41:42 +0530

    Update 28-09-23.java

--- 28-09-23.java
+++ 28-09-23.java
@@ -1,9 +1,7 @@
 class Solution {
     public static void convertToWave(int n, int[] a) {
         // code here
-        // arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....
-       /*From above sequence ,every element that is greater than its adjacent element 
-       has even index no. while others has odd index no.*/        
+        // arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....        
             for(int i=0;i<n-1;i++){
             if(a[i] < a[i+1] && i%2==0){ 
             // if current element index no is even and smaller than next element then swap both;
