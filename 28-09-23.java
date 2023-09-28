daa957466bde8252822a646ef08ee1189604880d
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-09-28 17:42:44 +0530

    Update 28-09-23.java

--- 28-09-23.java
+++ 28-09-23.java
@@ -7,7 +7,7 @@ class Solution {
                 a[i] = a[i+1];
                 a[i+1] = temp;
             }
-        // if currentelement index no is odd and larger than next element then swap both;
+
             else if(a[i] > a[i+1] && i%2 !=0){
                 int t = a[i];
                 a[i] = a[i+1];
