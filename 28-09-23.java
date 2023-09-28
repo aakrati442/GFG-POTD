b9eadba71ba40e5574421be4fdf6d4f3e86333f2
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-09-28 17:42:55 +0530

    Update 28-09-23.java

--- 28-09-23.java
+++ 28-09-23.java
@@ -2,7 +2,6 @@ class Solution {
     public static void convertToWave(int n, int[] a) {
             for(int i=0;i<n-1;i++){
             if(a[i] < a[i+1] && i%2==0){ 
-       
                 int temp = a[i];
                 a[i] = a[i+1];
                 a[i+1] = temp;
