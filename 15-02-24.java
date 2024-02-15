923fecb93740ddd494f37fc9ef106b1ed43955f4
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-02-15 12:33:48 +0530

    Create 15-02-24.java

---
+++ 15-02-24.java
@@ -0,0 +1,8 @@
+ for(int i=0;i<paths.length;i++){
+            int su=0;
+            for(int j=0;j<paths[i].length;j++){
+                if(paths[i][j]==1)su++;
+            }
+            if(su%2==1)return 0;
+        }
+        return 1;
