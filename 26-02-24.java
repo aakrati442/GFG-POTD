32a3e752561c2e8a63ecb4fe697d225cba703acb
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-02-26 12:20:41 +0530

    Create 26-02-24.java

---
+++ 26-02-24.java
@@ -0,0 +1,12 @@
+List<String> ans=new ArrayList<>();
+        trav(s.toCharArray(),0,s.length(),"",ans);
+        Collections.sort(ans);
+        return ans;
+    }
+public static void trav(char a[],int i,int n,String s,List<String> l){
+    if(i==n){
+        if(s.length()>0)l.add(s);
+        return;
+    }
+    trav(a,i+1,n,s+a[i],l);
+    trav(a,i+1,n,s,l);
