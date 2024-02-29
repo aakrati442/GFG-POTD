21a70c4e740b7058690478fcdf4a2f5cf5b59581
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-02-29 23:30:32 +0530

    Create 29-02-24.java

---
+++ 29-02-24.java
@@ -0,0 +1,21 @@
+long count = 0;
+
+        for (int i = 0; i < 32; i++) { 
+
+            int countSet = 0;
+
+            for (int num : arr) {
+
+                if ((num >> i & 1) == 1) {
+
+                    countSet++;
+
+                }
+
+            }
+
+            count += (countSet * (long) (n - countSet)) * 2; 
+
+        }
+
+        return count;
