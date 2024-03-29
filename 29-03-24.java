ab0f85d3f6f998bf7141e1d4d52a41dc696d6a58
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-03-29 12:02:42 +0530

    Create 29-03-24.java

---
+++ 29-03-24.java
@@ -0,0 +1,15 @@
+ vector<int> in(v,0);
+        
+        for(int i=0;i<v;i++){
+            
+            for(auto x: adj[i]){
+                in[x]++;
+            }
+        }
+        
+        for(int i=0;i<v;i++){
+            
+            if(in[i]==2 || in[i]==0)continue;
+            else return 0;
+        }
+        return 1;
