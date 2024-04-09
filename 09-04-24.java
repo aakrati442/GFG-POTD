109b4b2d7c2f2b5c016664fc5a9fd4a2a382f08f
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-04-09 11:18:38 +0530

    Create 09-04-24.java

---
+++ 09-04-24.java
@@ -0,0 +1,15 @@
+int[][] dp = new int[m][n];
+        dp[m-1][n-1] = Math.max(1, 1 - points[m-1][n-1]);
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1)
+                    continue; // Skip the destination cell
+                int minPointsRequired = Integer.MAX_VALUE;
+                if (i + 1 < m) // Move down
+                    minPointsRequired = Math.min(minPointsRequired, dp[i + 1][j]);
+                if (j + 1 < n) // Move right
+                minPointsRequired = Math.min(minPointsRequired, dp[i][j + 1]);
+                dp[i][j] = Math.max(1, minPointsRequired - points[i][j]);
+            }
+        } 
+        return dp[0][0];
