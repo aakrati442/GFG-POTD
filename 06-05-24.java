f18b5efc7daa1b1b6b49398c204e98e41cdb68cd
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-05-06 13:08:50 +0530

    Create 06-05-24.java

---
+++ 06-05-24.java
@@ -0,0 +1,23 @@
+ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> toReturn = helper(list,node);
+        if(toReturn.size()==0){
+            toReturn.add(-1);
+            return toReturn;
+        }
+        Collections.sort(toReturn);
+        return toReturn;
+    }
+    private static ArrayList<Integer> helper(ArrayList<Integer> list, Node root){
+        if(root==null){
+            return list;
+        }
+        if(root.left!=null && root.right==null){
+            list.add(root.left.data);
+        }
+        if(root.left==null && root.right!=null){
+            list.add(root.right.data);
+        }
+        helper(list,root.left);
+        helper(list,root.right);
+        
+        return list;
