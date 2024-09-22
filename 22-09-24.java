e15840ca6e7694b72f2d13b1c9fc8327ac0830e9
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2024-09-22 23:03:17 +0530

    Create 22-09-24.java

---
+++ 22-09-24.java
@@ -0,0 +1,18 @@
+int n=str.length();
+        
+        int p=0,s=1,pos=1,count=0;
+        
+        while(p<n && s<n){
+            if(str.charAt(p)==str.charAt(s)){
+                p++;
+                s++;
+                count++;
+            }
+            else{
+                p=0; //reset prefix pointer to start
+                pos++; //incement suffix start position
+                s=pos; //update suffix to new start position
+                count=0;
+            }
+        }
+        return count;
