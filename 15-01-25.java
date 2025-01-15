38ca2cf79aca7a1f7f15e76e33dcd4a1edf190e3
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2025-01-15 13:10:08 +0530

    Create 15-01-25.java

---
+++ 15-01-25.java
@@ -0,0 +1,15 @@
+HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0, -1);
+        int size=0;
+        int n=arr.length;
+        int sum=0;        
+        for(int i=0;i<n;i++) {
+            sum+=arr[i];
+            if(!map.containsKey(sum))map.put(sum,i);
+        
+            if(map.containsKey(sum-k)) {
+                int temp=i-map.get(sum-k);
+                size=Math.max(size,temp);
+            }
+        }        
+        return size;
