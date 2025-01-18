d2ec96f8b83c7c70cd06d8b3a80aa7d4c8ef0cf4
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2025-01-18 18:50:06 +0530

    Create 18-01-25.java

---
+++ 18-01-25.java
@@ -0,0 +1,9 @@
+Node prev = null;
+        Node cur = head;
+        while(cur != null){
+            Node next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
