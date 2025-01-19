de58250e7a2e9572610997ee3ceb0870c8c92013
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2025-01-19 23:06:47 +0530

    Create 19-01-25.java

---
+++ 19-01-25.java
@@ -0,0 +1,22 @@
+Node temp = head;
+        int size = 1;
+        while(temp.next != null){
+            size ++;
+            temp = temp.next;
+        }
+        
+        k = k%size;
+        if(k == 0){
+            return head;
+        }
+        
+        temp.next = head;
+        
+        Node newTail = head;
+        for(int i = 1; i < k; i++){
+            newTail = newTail.next;
+        }
+        
+        Node newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
