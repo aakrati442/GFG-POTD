60735744fc4c25e567f0fd28e9107a7a39f5ed06
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2025-02-11 23:23:24 +0530

    Create 11-02-25.java

---
+++ 11-02-25.java
@@ -0,0 +1,19 @@
+// code here.
+        ArrayList<Integer>vt=new ArrayList<>();
+        inorder(root,vt);
+        for(int i=1;i<vt.size();i++)
+        {
+            if(vt.get(i-1)>vt.get(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void inorder(Node root, ArrayList<Integer>vt)
+    {
+        if(root==null)
+        return;
+        inorder(root.left,vt);
+        vt.add(root.data);
+        inorder(root.right,vt);
