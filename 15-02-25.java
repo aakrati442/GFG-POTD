befe5129d99d342b36571520a3871df93570b161
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2025-02-15 21:56:19 +0530

    Create 15-02-25.java

---
+++ 15-02-25.java
@@ -0,0 +1,19 @@
+if(n1.data == root.data || n2.data == root.data)
+        {
+            return root;
+        }
+        
+        else if(n1.data < root.data && n2.data < root.data)
+        {
+            return(LCA(root.left,n1,n2));
+        }
+        
+         else if(n1.data > root.data && n2.data > root.data)
+        {
+            return(LCA(root.right,n1,n2));
+        }
+        
+        else
+        {
+            return root;
+        }
